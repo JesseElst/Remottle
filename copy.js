422cd3112b64d99cf3e75c2c342116f34f77edec
Klaasjan <jesse656@gmail.com>
2018-01-19 14:35:32 +0100

    comments toegevoegd + sql injection tegengegaan

--- copy.js
+++ copy.js
@@ -1,9 +1,9 @@
-var mqtt = require('mqtt');
-var Topic = 'public/i363602_water/tilt/'; //subscribe to all topics
+var mqtt = require('mqtt'); 
+var Topic = 'public/i363602_water/tilt/'; // subscribe op deze topic
+var Broker_URL = 'mqtt://mqtt.fhict.nl'; //zoek op deze url
+var Database_URL = 'localhost';//zoek database op de localhost
 
-var Broker_URL = 'mqtt://mqtt.fhict.nl';
-var Database_URL = 'localhost';
-
+//voer gegevens in om in te loggen
 var options = {
 	clientId: 'i363602_water',
 	port: 1883,
@@ -12,6 +12,7 @@ var options = {
 	keepalive : 60
 };
 
+// maak verbinding om de handelingen uit te voeren
 var client  = mqtt.connect(Broker_URL, options);
 client.on('connect', mqtt_connect);
 client.on('reconnect', mqtt_reconnect);
@@ -19,42 +20,52 @@ client.on('error', mqtt_error);
 client.on('message', mqtt_messsageReceived);
 client.on('close', mqtt_close);
 
+//maakt verbinding met MQTT
 function mqtt_connect() {
     console.log("Connecting MQTT");
     client.subscribe(Topic, mqtt_subscribe);
 }
 
+//subsribe op de topic
 function mqtt_subscribe(err, granted) {
     console.log("Subscribed to " + Topic);
     if (err) {console.log(err);}
 }
 
+//maak opnieuw verbinding 
 function mqtt_reconnect(err) {
     console.log("Reconnect MQTT");
     if (err) {console.log(err);}
 	client  = mqtt.connect(Broker_URL, options);
 }
 
+//geef een error als er een fout is
 function mqtt_error(err) {
     console.log("Error!");
 	if (err) {console.log(err);}
 }
 
-function mqtt_messsageReceived(topic, message, packet) {
+//stuur het ontvangen bericht naar een database
+function mqtt_messsageReceived(message) {
 	console.log('Message received = ' + message);
     console.log('test');
-	insert_message(topic, message, packet);
+	insert_message(message);
 }
 
+//stop verbinding MQTT
 function mqtt_close() {
 	console.log("Close MQTT");
 }
-////////////////////////////////////////////////////
+
+
+
+
 ///////////////////// MYSQL ////////////////////////
-////////////////////////////////////////////////////
+
+
 var mysql = require('mysql');
 
-//Create Connection
+//maak verbinding met database
 var connection = mysql.createConnection({
 	host: Database_URL,
 	user: "root",
@@ -67,18 +78,19 @@ connection.connect(function(err) {
 	console.log("Database Connected!");
 });
 
-//insert a row into the tbl_messages table
-function insert_message(topic, message, packet) {
-    
-	var clientID= "client001";
+//voeg het bericht toe aan de database
+function insert_message(message) {
+    //laat zien welke dag het is
     var d = new Date();
     var dag = d.getDate();
     var maand = d.getMonth() +1 ;
     var jaar = d.getFullYear();
     var volledig = dag+ "" + maand + "" + jaar;
     console.log(volledig);
-	var sql = "INSERT INTO fles (water) VALUES ('" + message + "')";
-	var params = ['tbl_messages', 'clientID', 'topic', 'message', clientID, topic, message];
+    //voorgedefinieerd statement om sql injection tegen te gaan
+	var sql = "INSERT INTO ?? (??) VALUES (??)";
+    //waardes die in de sql code ingevoerd worden
+	var params = ['fles', 'water', message];
 	sql = mysql.format(sql, params);	
 	connection.query(sql, function (error, results) {
 		if (error) throw error;
